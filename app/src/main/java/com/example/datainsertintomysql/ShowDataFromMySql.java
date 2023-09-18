1f54b8680b4b9fd8e5aa809799b59329cecf54d5
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-09-18 17:11:06 +0600

    update & delete done

--- app/src/main/java/com/example/datainsertintomysql/ShowDataFromMySql.java
+++ app/src/main/java/com/example/datainsertintomysql/ShowDataFromMySql.java
@@ -2,12 +2,16 @@ package com.example.datainsertintomysql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -18,6 +22,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -40,49 +45,8 @@ public class ShowDataFromMySql extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         progressBar = findViewById(R.id.progressBar);
 
+        loadData();
 
-        String url = "https://maruf5682.000webhostapp.com/apps/view.php";
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        progressBar.setVisibility(View.GONE);
-
-                        try {
-                            for (int i = 0; i<response.length();i++){
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                String name = jsonObject.getString("name");
-                                String phone = jsonObject.getString("phone");
-                                String email = jsonObject.getString("email");
-
-                                hashMap = new HashMap<>();
-                                hashMap.put("name",name);
-                                hashMap.put("phone",phone);
-                                hashMap.put("email",email);
-                                arrayList.add(hashMap);
-
-                                Toast.makeText(ShowDataFromMySql.this, email, Toast.LENGTH_SHORT).show();
-                            }
-
-                            MyAdapter myAdapter = new MyAdapter();
-                            listView.setAdapter(myAdapter);
-                            Toast.makeText(ShowDataFromMySql.this, "successfully done", Toast.LENGTH_SHORT).show();
-
-                        } catch (JSONException e) {
-                            Log.d("errorCatch", String.valueOf(e));
-                            Toast.makeText(ShowDataFromMySql.this, "errorCatch", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d("error", String.valueOf(error));
-                Toast.makeText(ShowDataFromMySql.this, "error", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        RequestQueue requestQueue = Volley.newRequestQueue(ShowDataFromMySql.this);
-        requestQueue.add(jsonArrayRequest);
     }
 
     private class MyAdapter extends BaseAdapter{
@@ -106,21 +70,159 @@ public class ShowDataFromMySql extends AppCompatActivity {
         public View getView(int i, View view, ViewGroup viewGroup) {
             LayoutInflater layoutInflater = getLayoutInflater();
             View myView = layoutInflater.inflate(R.layout.user_info,null);
-            TextView nameText, emailText, phoneText;
+            TextView nameText, emailText, phoneText,idText;
+            Button updateBtn,deleteBtn;
+            idText = myView.findViewById(R.id.id);
             nameText = myView.findViewById(R.id.nameS);
             emailText = myView.findViewById(R.id.email);
             phoneText = myView.findViewById(R.id.phone);
+            updateBtn = myView.findViewById(R.id.update);
+            deleteBtn = myView.findViewById(R.id.delete);
 
             HashMap<String,String>hashMap1 = arrayList.get(i);
+            String id = hashMap1.get("id");
             String name = hashMap1.get("name");
             String phone = hashMap1.get("phone");
             String email = hashMap1.get("email");
 
+            idText.setText(id);
             nameText.setText(name);
             phoneText.setText(phone);
             emailText.setText(email);
 
+            updateBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Dialog updateDialog = new Dialog(ShowDataFromMySql.this);
+                    updateDialog.setContentView(R.layout.update_dialog);
+                    updateDialog.setCancelable(true);
+                    updateDialog.show();
+
+
+                    EditText editName = updateDialog.findViewById(R.id.editName);
+                    EditText editNumber = updateDialog.findViewById(R.id.editNumber);
+                    EditText editEmail = updateDialog.findViewById(R.id.editEmail);
+                    Button updateSave = updateDialog.findViewById(R.id.updateSave);
+
+
+
+                    updateSave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                           String phone = editNumber.getText().toString();
+                           String email = editEmail.getText().toString();
+                           String name = editName.getText().toString();
+                            String url = "https://maruf5682.000webhostapp.com/apps/update.php?id="+ id +"&name="+ name +"&email="+ email +"&phone="+ phone;
+
+                            progressBar.setVisibility(View.VISIBLE);
+                            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                                    new Response.Listener<String>() {
+                                        @Override
+                                        public void onResponse(String response) {
+                                            progressBar.setVisibility(View.GONE);
+                                            new AlertDialog.Builder(ShowDataFromMySql.this)
+                                                    .setTitle("Server Response")
+                                                    .setMessage(response)
+                                                    .show();
+                                            loadData();
+                                            updateDialog.dismiss();
+                                        }
+                                    }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+
+                                }
+                            });
+
+                            RequestQueue requestQueue = Volley.newRequestQueue(ShowDataFromMySql.this);
+                            requestQueue.add(stringRequest);
+                        }
+                    });
+
+
+
+                }
+            });
+
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(ShowDataFromMySql.this, "Delete Successfully", Toast.LENGTH_SHORT).show();
+                    String url = "https://maruf5682.000webhostapp.com/apps/delete.php?id="+ id;
+
+                    StringRequest deleteRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    new AlertDialog.Builder(ShowDataFromMySql.this)
+                                            .setTitle("Server Response")
+                                            .setMessage(response)
+                                            .show();
+                                    loadData();
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(ShowDataFromMySql.this, "Delete Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    RequestQueue requestQueue = Volley.newRequestQueue(ShowDataFromMySql.this);
+                    requestQueue.add(deleteRequest);
+                }
+            });
+
             return myView;
         }
+    }
+
+
+    private void loadData(){
+        arrayList = new ArrayList<>();
+
+        String url = "https://maruf5682.000webhostapp.com/apps/view.php";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        progressBar.setVisibility(View.GONE);
+
+                        try {
+                            for (int i = 0; i<response.length();i++){
+                                JSONObject jsonObject = response.getJSONObject(i);
+                                String id = jsonObject.getString("id");
+                                String name = jsonObject.getString("name");
+                                String phone = jsonObject.getString("phone");
+                                String email = jsonObject.getString("email");
+
+                                hashMap = new HashMap<>();
+                                hashMap.put("id",id);
+                                hashMap.put("name",name);
+                                hashMap.put("phone",phone);
+                                hashMap.put("email",email);
+                                arrayList.add(hashMap);
+
+
+                            }
+
+                            MyAdapter myAdapter = new MyAdapter();
+                            listView.setAdapter(myAdapter);
+                            Toast.makeText(ShowDataFromMySql.this, "successfully done", Toast.LENGTH_SHORT).show();
+
+                        } catch (JSONException e) {
+                            Log.d("errorCatch", String.valueOf(e));
+                            Toast.makeText(ShowDataFromMySql.this, "errorCatch", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", String.valueOf(error));
+                Toast.makeText(ShowDataFromMySql.this, "error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(ShowDataFromMySql.this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
